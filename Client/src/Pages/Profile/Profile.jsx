45577dc936f1c573bb92ea3eb727a0bf1abe90e1
NicknameIsAvailable <iamgenii@yandex.ru>
2023-01-27 09:18:56 +0300

    profile

--- Client/src/Pages/Profile/Profile.jsx
+++ Client/src/Pages/Profile/Profile.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import "./Profile.css";
+import Notification from "./Components/Notification/Notification";
 
 const Profile = () => {
+    const notifications = [
+        {
+            date: "12.12.12",
+            time: "12:12",
+            content: "Бебрик И.И. выложил новое задание в Google classroom"
+        },
+        {
+            date: "13.11.11",
+            time: "02:11",
+            content: "Невероятный текст уведомления"
+        },
+        {
+            date: "1.1.1",
+            time: "23:34",
+            content: "вавдоаылвоадлова"
+        },
+    ]
+
     return (
         <div className="ProfilePage">
             <h1>ПРОФИЛЬ</h1>
             <div className="profile-block">
-                    
+                <div className="notifications-block">
+                    <h2 className="notifications-title">Уведомления</h2>
+                    {
+                        notifications.map(notification =>
+                            <Notification
+                                date={notification.date}
+                                time={notification.time}
+                                content={notification.content}
+                            />
+                        )
+                    }
+                </div>
             </div>
         </div>
     );
